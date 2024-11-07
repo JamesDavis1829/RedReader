35f5a7b5249f982c376f4fe821d64db470aaef7c
James Davis <jedwarddavis902@gmail.com>
2024-11-07 02:38:45 +0000

    Update MongoDB connection to use environment variable for URI

--- db/mongo.go
+++ db/mongo.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -12,7 +13,7 @@ func NewMongoClient() *mongo.Client {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
 	if err != nil {
 		panic(err)
 	}
